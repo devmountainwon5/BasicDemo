587e4dc0b71b54577f3945b197f611b8acff7b7b
Joshua Smith <veryjoshua@gmail.com>
2019-04-03 23:02:09 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import MyFirstComponent from './myFirstComponent';
+
+class App extends Component {
+  render() {
+    
+    return (
+      <div className="App">
+        <MyFirstComponent/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/myFirstComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MyFirstComponent extends Component {
+  render() {
+    return (
+        <div>
+            1
+        </div>
+    );
+  }
+}
+
+export default MyFirstComponent;
